51836305c6764f11ef1acabffa9b183783e554f7
chazzysb <cballagan@yahoo.com>
2023-01-27 10:20:54 +0000

    FAI-750-UpgradeToDotNetSix

---
+++ src/SFA.DAS.Functions.Importer.MockAPIs/MockApiBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using WireMock.Logging;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+
+namespace SFA.DAS.Functions.Importer.MockAPIs;
+
+public class MockApiBuilder
+{
+    private readonly WireMockServer _server;
+
+    public MockApiBuilder(int port)
+    {
+        _server = WireMockServer.Start(new WireMockServerSettings
+        {
+            Port = port,
+            UseSSL = true,
+            StartAdminInterface = true,
+            Logger = new WireMockConsoleLogger(),
+        });
+    }
+
+    public static MockApiBuilder Create(int port)
+    {
+        return new MockApiBuilder(port);
+    }
+
+    public MockApi Build()
+    {
+        return new MockApi(_server);
+    }
+
+    public MockApiBuilder StartEndPoints()
+    {
+        _server.Given(Request.Create()
+                        .WithPath("/ops/dataload")
+                        .UsingPost())
+               .RespondWith(Response.Create()
+                        .WithStatusCode(HttpStatusCode.OK)
+                        .WithBodyAsJson($"called /ops/dataload on port {_server.Port}"));
+
+        return this;
+    }
+}
---
+++ src/SFA.DAS.Functions.Importer.MockAPIs/Program.cs
@@ -0,0 +1,8 @@
+﻿using SFA.DAS.Functions.Importer.MockAPIs;
+
+MockApiBuilder.Create(5001).StartEndPoints().Build();
+MockApiBuilder.Create(5006).StartEndPoints().Build();
+MockApiBuilder.Create(5008).StartEndPoints().Build();
+
+Console.WriteLine("Press any key to stop the servers");
+Console.ReadKey();
